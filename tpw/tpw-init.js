9f08b8e3d6237bcdcb6f9526d8eae8f0669e1030
Travis Simpson <travis@codegoat.com>
2012-05-08 01:07:04 -0700

    Changes to the tpw-init script so that it loads all libs/widgets relative to the path it is included from (more CDN friendly).

--- tpw/tpw-init.js
+++ tpw/tpw-init.js
@@ -1,5 +1,5 @@
 TPW = function () {
-  var self = this;
+  var self = this, path;
   this.init = function () {
     this.widgets = [];
     (function (e, a, g, h, f, c, b, d) {
@@ -17,10 +17,10 @@ TPW = function () {
         }
     }(window, document, "1.7.2", function ($, L) {
       self.jQuery = $;
-      loadScript('tpw/libs/jshashtable-2.1.js', function () {
-        loadScript('tpw/libs/jquery.numberformatter-1.2.3.min.js', function () {
+      loadScript('libs/jshashtable-2.1.js', function () {
+        loadScript('libs/jquery.numberformatter-1.2.3.min.js', function () {
           loadScript('http://cdnjs.cloudflare.com/ajax/libs/datejs/1.0/date.min.js', function () {
-            loadScript('tpw/libs/jquery.jsonp-2.3.0.min.js', function () {
+            loadScript('libs/jquery.jsonp-2.3.0.min.js', function () {
               loadScript('http://cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.0.0.beta2/handlebars.min.js', function () {
                 registerHandlebarHelpers();
                 self.loaded = true;
@@ -35,7 +35,7 @@ TPW = function () {
   this.load = function (js, options) {
     if (this.loaded) {
       var config = options || {};
-      loadScript('tpw/widgets/' + js + '.js', function () {
+      loadScript('widgets/' + js + '.js', function () {
         window['_' + js].init(self, config);
         self.widgets.push(window['_' + js]);
       });
@@ -47,7 +47,11 @@ TPW = function () {
   var loadScript = function (script, callback) {
     var c = document.createElement("script");
     c.type = "text/javascript";
-    c.src = script;
+    if (script.substr(0, 4) == 'http') {
+      c.src = script;
+    } else {
+      c.src = self.path + script;
+    }
     c.onload = c.onreadystatechange = function () {
       if (!(d = this.readyState) || d === "loaded" || d === "complete") {
         if (typeof callback === 'function') {
@@ -66,3 +70,5 @@ TPW = function () {
 };
 
 var _TPW = new TPW();
+var pageScriptTags = document.getElementsByTagName("script"), tpwSrc = pageScriptTags[pageScriptTags.length-1].src;
+_TPW.path = tpwSrc.substr(0, tpwSrc.indexOf('tpw-init.js'));
